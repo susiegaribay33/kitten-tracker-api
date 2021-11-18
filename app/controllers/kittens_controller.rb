efb29554f5aa66fbb23da439ec5da6bd51705120
SusieG <susiegaribay33@gmail.com>
2021-11-17 18:44:18 -0600

    add serializer, index action

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,3 +1,8 @@
 class KittensController < ApplicationController
-  
+
+  def index
+    all_kittens = Kitten.all
+    render json: all_kittens.as_json
+  end
+
 end
---
+++ app/serializers/kitten_serializer.rb
@@ -0,0 +1,3 @@
+class KittenSerializer < ActiveModel::Serializer
+  attributes :id, :name, :color, :age
+end
