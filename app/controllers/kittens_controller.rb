5066215a85f7bec34a4dd7d58e7d393a1ac1bac7
SusieG <susiegaribay33@gmail.com>
2021-11-17 21:35:19 -0600

    add create, show, update

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -5,4 +5,28 @@ class KittensController < ApplicationController
     render json: all_kittens.as_json
   end
 
+  def create
+    new_kitten = Kitten.new(
+      name: params[:name],
+      color: params[:color],
+      age: params[:age]
+    )
+    new_kitten.save
+    render json: new_kitten
+  end
+
+  def show
+    the_kitten = Kitten.find(params[:id])
+    render json: the_kitten
+  end
+
+  def update
+    the_kitten = Kitten.find(params[:id])
+    the_kitten.name = params[:name] || the_kitten.name
+    the_kitten.color = params[:color] || the_kitten.color
+    the_kitten.age = params[:age] || the_kitten.age
+    the_kitten.save
+    render json: the_kitten
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/kittens" => "kittens#index"
+  post "/kittens" => "kittens#create"
+  get "/kittens/:id" => "kittens#show"
+  patch "/kittens/:id" => "kittens#update"
 end
